8b17a88a139317c326df4aacf8aeacb7d2f4b703
Stacy <stacyxberlin@gmail.com>
2025-04-28 15:21:41 +0200

    part 1-A

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -2,13 +2,14 @@ import {createSlice} from "@reduxjs/toolkit";
 import {signUp} from './operations.js'
 
 const slice = createSlice({
+    name: 'user',
     initialState: {
         name:'',
         email: '',
         password: ''
     },
     extraReducers: (builder)=>{
-        builder.addCase(signUp.fullfilled, (state, action)=>{
+        builder.addCase(signUp.fulfilled, (state, action)=>{
             state = action.payload
         })
     }
