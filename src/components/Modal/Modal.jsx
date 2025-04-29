d45558b9eb2c9f6aaf55b446cdd8ecdda2961e61
Stacy <stacyxberlin@gmail.com>
2025-04-29 18:21:55 +0200

    rest

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -27,6 +27,9 @@ const Modal = () => {
     dispatch(changeData({ ...value, id: id }));
     dispatch(changeContact(null));
   };
+  const handleClick = () => {
+    dispatch(changeContact(null));
+  };
   return (
     <div
       className="relative z-10"
@@ -96,6 +99,7 @@ const Modal = () => {
                         </button>
                         <button
                           type="button"
+                          onClick={handleClick}
                           className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
                         >
                           Cancel
